195291b5beed64e978aea0cae4897ab5cd618ffa
petrl <sup686@gmail.com>
2024-12-27 11:02:06 +0300

    ref

--- src/db/base.py
+++ src/db/base.py
@@ -1,8 +1,11 @@
-from typing import Any
+from contextlib import asynccontextmanager
+from typing import Any, List
 
 from fastapi import HTTPException, status
+from sqlalchemy import update, and_
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 from sqlalchemy.orm import declared_attr, DeclarativeBase
 
 
@@ -15,39 +18,66 @@ class Base(DeclarativeBase):
         """Автоматически генерировать имя таблицы из имени класса."""
         return self.__name__.lower()
 
-    # Добавление экземпляра в базу данных
-    async def add(self, db_session: AsyncSession):
+    @classmethod
+    async def get_all(cls, db: AsyncSession, *conditions) -> List[Any]:
+        """
+        Получить записи с использованием сложных фильтров.
+        records = await TasksBase.get_all(db, TasksBase.stage == 3, TasksBase.status == 0)
+        """
         try:
-            db_session.add(self)
-            await db_session.commit()
+            stmt = select(cls)
+            if conditions:
+                stmt = stmt.filter(and_(*conditions))
+            result = await db.execute(stmt)
+            return list(result.scalars().all())
         except SQLAlchemyError as ex:
-            await db_session.rollback()
             raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail=str(ex)
             ) from ex
 
-    # Удаление экземпляра из базы данных
-    async def delete(self, db_session: AsyncSession):
+    @classmethod
+    async def update_where(cls, db: AsyncSession, values: dict | Any, *conditions):
+        """
+        Обновить записи с использованием сложных фильтров.
+        await TasksBase.update_where(db, {"status": 1}, TasksBase.stage == 3, TasksBase.status == 0)
+        """
         try:
-            await db_session.delete(self)
-            await db_session.commit()
-        except SQLAlchemyError as ex:
-            await db_session.rollback()
-            raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                detail=repr(ex)
-            ) from ex
-
-    # Обновление полей экземпляра
-    async def update(self, db: AsyncSession, **kwargs):
-        try:
-            for k, v in kwargs.items():
-                setattr(self, k, v)
+            stmt = update(cls).where(and_(*conditions)).values(**values)
+            await db.execute(stmt)
             await db.commit()
         except SQLAlchemyError as ex:
             await db.rollback()
             raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                detail=repr(ex)
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=str(ex)
             ) from ex
+
+    @asynccontextmanager
+    async def transaction(self, db_session: AsyncSession):
+        """Контекстный менеджер для управления транзакциями."""
+        try:
+            yield
+            await db_session.commit()
+        except SQLAlchemyError as ex:
+            await db_session.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=str(ex)
+            ) from ex
+
+    async def add(self, db_session: AsyncSession):
+        """Добавить запись в базу данных."""
+        # async with self.transaction(db_session):
+        db_session.add(self)
+
+    async def delete(self, db_session: AsyncSession):
+        """Удалить запись из базы данных."""
+        async with self.transaction(db_session):
+            await db_session.delete(self)
+
+    async def update(self, db: AsyncSession, **kwargs):
+        """Обновить запись."""
+        async with self.transaction(db):
+            for k, v in kwargs.items():
+                setattr(self, k, v)
