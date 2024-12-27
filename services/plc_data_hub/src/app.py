9b4f94fa2064d864802f7eb45db8fdcfa50af2c9
petrl <sup686@gmail.com>
2024-12-27 17:37:11 +0300

    fix plc_reader

--- services/plc_data_hub/src/app.py
+++ services/plc_data_hub/src/app.py
@@ -1,35 +1,32 @@
 import asyncio
 import logging
 
-from services.plc_data_hub.src.utils.utils import fetch_places
-from services.plc_data_hub.src.core.plc_reader import Reader
-from shared.db.manufactory.database import AsyncSessionFactory
+# from services.plc_data_hub.src.core.plc_reader import Reader
+from services.plc_data_hub.src.core.reader import Reader
+from services.plc_data_hub.src.utils import get_places_with_name_containing
 from shared.logger import logger
 
-logging.getLogger("main").setLevel(logging.WARNING)
+logging.getLogger("app").setLevel(logging.WARNING)
 
 
 async def initialize_readers():
-    async with AsyncSessionFactory() as session:  # Создаём реальную сессию базы данных
-        places = await fetch_places()  # Получаем список PLC
+    places = await get_places_with_name_containing("Кромочник")
 
-        if not places:
-            logger.info("Нет подходящих PLC для подключения.")
-            return
+    if not places:
+        logger.info("Нет подходящих PLC для подключения.")
+        return
 
-        tasks = []
-        for place in places:
-            logger.info(f"Инициализация Reader для {place.name} ({place.ip})")
+    tasks = []
+    for place in places:
+        logger.info(f"Инициализация Reader для {place.name} ({place.ip})")
 
-            # Передаём реальную сессию
-            async with AsyncSessionFactory() as place_session:
-                reader = Reader(ip=place.ip, db_session=place_session)
-                tasks.append(reader.run())
+        reader = Reader(ip=place.ip)
+        tasks.append(reader.run())
 
-        results = await asyncio.gather(*tasks, return_exceptions=True)
-        for result in results:
-            if isinstance(result, Exception):
-                logger.error(f"Ошибка в задаче: {result}")
+    results = await asyncio.gather(*tasks, return_exceptions=True)
+    for result in results:
+        if isinstance(result, Exception):
+            logger.error(f"Ошибка в задаче: {result}")
 
 
 if __name__ == "__main__":
--- services/plc_data_hub/src/utils/utils.py
+++ services/plc_data_hub/src/utils/utils.py
@@ -2,26 +2,17 @@ import json
 from hashlib import md5
 
 from shared.db.manufactory.database import AsyncSessionFactory
-from shared.db.manufactory.models.models import Place
+from shared.db.manufactory.models import Place
 
 
-async def _get_filtered_places(session):
+async def get_places_with_name_containing(keyword: str):
     """
-    Фильтрует записи Place, которые содержат IP-адрес и определенную подстроку в имени.
-    :param session: Сессия базы данных.
+    Фильтрует записи Place, которые содержат определенный keyword в названии.
+    :param keyword: Строка с частью названия.
     :return: Список объектов Place, соответствующих условиям.
     """
-    places = await Place.get_all(session)
-    return [place for place in places if place.ip and 'Кромочник' in place.name]
-
-
-async def fetch_places():
-    """
-    Получает список PLC с подходящими именами и IP-адресами.
-    :return: Список объектов Place.
-    """
     async with AsyncSessionFactory() as session:
-        return await _get_filtered_places(session)
+        return await Place.get_all(session, Place.name.like(f"%{keyword}%"))
 
 
 def compute_hash(value):
