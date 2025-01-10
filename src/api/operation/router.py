9c0c991cba47c463e911ca35cb902841d95864b1
petrl <sup686@gmail.com>
2025-01-10 07:51:03 +0300

    fix operation_history add

--- src/api/operation/router.py
+++ src/api/operation/router.py
@@ -1,17 +1,15 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import APIRouter, HTTPException
 
 from src.api.operation.schemas import OperationHistory
 from src.api.operation.services import add_operation_history
-from src.database import get_db
 
 router = APIRouter()
 
 
 @router.post("/add", name="add_operation_history")
-async def add_operation_history_endpoint(operation_history_data: OperationHistory, db: AsyncSession = Depends(get_db)):
+async def add_operation_history_endpoint(operation_history_data: OperationHistory):
     try:
-        await add_operation_history(db, operation_history_data)
+        await add_operation_history(operation_history_data)
         return operation_history_data
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- src/api/operation/services.py
+++ src/api/operation/services.py
@@ -1,16 +1,17 @@
 from datetime import datetime
 
-from sqlalchemy.ext.asyncio import AsyncSession
-
+from shared.db.manufactory.database import AsyncSessionFactory
+from shared.db.manufactory.models import OperationHistory as OperationHistoryDB
 from src.api.operation.schemas import OperationHistory as OperationHistorySchema
-from src.db.models import OperationHistory as OperationHistoryDB
 
 
-async def add_operation_history(db: AsyncSession, operation_history_data: OperationHistorySchema):
-    new_operation = OperationHistoryDB(
-        place_id=operation_history_data.place,
-        program=operation_history_data.program,
-        text=operation_history_data.data if operation_history_data.data else "",
-        dt_created=datetime.now(),
-    )
-    await new_operation.add(db)
+async def add_operation_history(operation_history_data: OperationHistorySchema):
+    async with AsyncSessionFactory() as session:
+        async with session.begin():
+            new_operation = OperationHistoryDB(
+                place_id=operation_history_data.place,
+                program=operation_history_data.program,
+                text=operation_history_data.data if operation_history_data.data else "",
+                dt_created=datetime.now(),
+            )
+            await new_operation.add(session)
